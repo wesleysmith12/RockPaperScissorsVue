0b23e73acd5d012623af2d57d4b786af6b456d0d
Wesley <wesleysmith1@mail.weber.edu>
2017-10-01 22:47:24 -0600

    twist 2 completed

--- RPS VueJS Template/app.js
+++ RPS VueJS Template/app.js
@@ -8,137 +8,249 @@ new Vue({
     width: 0,
     userGuess: "",
     computerGuess: "",
-    log: []
+    log: [],
+    computerMoves: [],
+    userMoves: [],
+    gameTime: 0,
+    guessIndex: 100,
+    computerThrowCount: 100,
+    count: 0,
+    highScoreRounds: -1,
+    highScoreTime: -1,
+    timeouts: [],
+    gameStarted: false
   },
   methods: {
-    test: function(){
-      //alert("hello world");
-    },
     startNew: function(){
 
+        // show green completion bars
+        this.gameStarted = true;
+
+        // reset variables
+        this.guessIndex = 0;
+        this.computerThrowCount = 0;
+
+        // user start time
+        this.gameTime = new Date().getTime() / 1000;
+
+        // hide start button and show buttons
         document.getElementById('startbtn').style.display = 'none';
         document.getElementById('gamebtns').style.display = 'block';
 
-        //alert("Test");
+        //show div that displays computer generated choice
+        document.getElementById('computerMoves').style.display = 'block';
+
+        // generate computer throw
+        this.generateThrow();
+
+        //display throws
+        this.getThrows();
+
+    },
+    generateThrow: function(){
+
+        // generate random number for computer throw
+        this.computerMoves.push(Math.floor((Math.random() * 3) + 1));
+
+        // clear so user has to re-enter what computer generated
+        this.userMoves = [];
+
+        // add to total computer throws
+        this.computerThrowCount++;
+
+    },
+    getThrows: function(){
+        for(var i = 0; i < this.computerMoves.length; i++){
+
+            // display throws at one second intervals
+            this.timeouts.push(setTimeout(this.display, i*1000, this.computerMoves[i], i+1));
+        }
+
+    },
+    display: function(computerThrow, throwNum){
+
+        //display the computer's throws
+        switch(computerThrow) {
+            case 1:
+                document.getElementById('image').src='rock.jpg';
+                break;
+            case 2:
+                document.getElementById('image').src='paper.jpg';
+                break;
+            case 3:
+                document.getElementById('image').src='scissors.jpg';
+                break;
+            default:
+                alert("there was an error with the computer generated throw");
+        }
+
+        this.count = throwNum;
+
+    },
+    compareThrows: function(){
+
+        this.guessIndex++;
+
+        // compare user choices to computer choices
+        for(var i = 0; i < this.userMoves.length; i++){
+
+            if(this.userMoves[i] == 1){
+
+                if(this.computerMoves[i] == 3){
+                    //user selected correct option
+                    if(this.userMoves.length == this.computerMoves.length){
+                        this.timeouts.push(setTimeout(this.resetGuessIndex, 1000));
+                        return;
+                    }
+
+                }else{
+                    //user selected wrong option
+                    this.gameTime = (new Date().getTime() / 1000) - this.gameTime;
+                    //alert("wrong User Move: " + this.userMoves[this.userMoves.length-1] + " Computer Move: " + this.computerMoves[this.computerMoves.length-1]);
+                    this.updateLog(2);
+                    this.checkHighScore();
+                    this.guessIndex = 0;
+                    return;
+                }
+            }else if(this.userMoves[i] == 2){
+                if(this.computerMoves[i] == 1){
+                    //user selected correct option
+                    if(this.userMoves.length == this.computerMoves.length){
+                        this.timeouts.push(setTimeout(this.resetGuessIndex, 1000));
+                        return;
+                    }
+
+                }else{
+                    //user selected wrong option
+                    this.gameTime = (new Date().getTime() / 1000) - this.gameTime;
+                    //alert("wrong User Move: " + this.userMoves[this.userMoves.length-1] + " Computer Move: " + this.computerMoves[this.computerMoves.length-1]);
+                    this.updateLog(2);
+                    this.checkHighScore();
+                    this.guessIndex = 0;
+                    return;
+                }
+            }else{
+                if(this.computerMoves[i] == 2){
+                    //user selected correct option
+                    if(this.userMoves.length == this.computerMoves.length){
+                        this.timeouts.push(setTimeout(this.resetGuessIndex, 1000));
+                        return;
+                    }
+
+                }else{
+                    //user selected wrong option
+                    this.gameTime = (new Date().getTime() / 1000) - this.gameTime;
+                    //alert("wrong User Move: " + this.userMoves[this.userMoves.length-1] + " Comupter Move: " + this.computerMoves[this.computerMoves.length-1]);
+                    this.updateLog(2);
+                    this.checkHighScore();
+                    this.guessIndex = 0;
+                    return;
+                }
+            }
+
+        }
+
+    },
+    resetGuessIndex: function(){
+        this.guessIndex = 0;
+        this.generateThrow();
+        this.getThrows();
+        this.updateLog(1);
+    },
+    updateLog: function(x){
+        if(x == 1){
+            this.log.unshift({
+                msg:"COMPLETED ROUND " + (this.computerMoves.length -1),
+                playerwin: true
+            });
+        }else{
+            this.log.unshift({
+                msg:"GAME OVER | ROUNDS COMPLETED " + (this.computerThrowCount - 1),
+                playerwin: false
+            });
+        }
+
     },
     rock: function(){
-        var computer = Math.floor((Math.random() * 3) + 1);
 
-        if(computer == 1){
-            this.ties++;
-            this.gameCount++;
-            this.log.unshift({
-                msg:"COMPUTER CHOSE ROCK | YOU HAVE TIED!",
-                playertie: true
-            });
+        // record user choice
+        this.userMoves.push(1);
 
-        }else if(computer == 2){
-            this.computerWins++;
-            this.gameCount++;
+        this.compareThrows();
 
-            this.log.unshift({
-                msg:"COMPUTER CHOSE PAPER | PAPER BEATS ROCK | COMPUTER WINS!",
-                playerwin: false
-            });
-
-        }else{
-            this.userWins++;
-            this.gameCount++;
-
-            this.log.unshift({
-                msg:"COMPUTER CHOSE SCISSORS | ROCK BEATS SCISSORS | PLAYER WINS!",
-                playerwin: true
-            });
-        }
-
-        this.checkWins();
     },
     paper: function(){
-        
-        var computer = Math.floor((Math.random() * 3) + 1);
 
-        if(computer == 1){
-            this.userWins++;
-            this.gameCount++;
+        // record user choice
+        this.userMoves.push(2);
 
-            this.log.unshift({
-                msg:"COMPUTER CHOSE ROCK | PAPER BEATS ROCK | PLAYER WINS!",
-                playerwin: true
-            });
-
-        }else if(computer == 2){
-            this.ties++;
-            this.gameCount++;
-
-            this.log.unshift({
-                msg:"COMPUTER CHOSE PAPER | YOU HAVE TIED!",
-                playertie: true
-            });
-
-        }else{
-            this.computerWins++;
-            this.gameCount++;
-
-            this.log.unshift({
-                msg:"COMPUTER CHOSE SCISSORS | SCISSORS BEATS PAPER | COMPUTER WINS!",
-                playerwin: false
-            });
-        }
-
-        this.checkWins();
+        this.compareThrows();
 
     },
     scissors: function(){
 
-        var logMsg = "";
+        // record user choice
+        this.userMoves.push(3);
 
-        var computer = Math.floor((Math.random() * 3) + 1);
-
-        if(computer == 1){
-            this.computerWins++;
-            this.gameCount++;
-
-            this.log.unshift({
-                msg:"COMPUTER CHOSE ROCK | ROCK BEATS SCISSORS | COMPUTER WINS!",
-                playerwin: false
-            });
-
-        }else if(computer == 2){
-            this.userWins++;
-            this.gameCount++;
-
-            this.log.unshift({
-                msg:"COMPUTER CHOSE PAPER | SCISSORS BEATS ROCK | PLAYER WINS!",
-                playerwin: true
-            });
-
-        }else{
-            this.ties++;
-            this.gameCount++;
-
-            this.log.unshift({
-                msg:"COMPUTER CHOSE SCISSORS | YOU HAVE TIED!",
-                playertie: true
-            });
-
-        }
-
-        this.checkWins();
+        this.compareThrows();
 
     },
     restart: function(){
 
+        // disable timeouts
+        for (var i = 0; i < this.timeouts.length; i++) {
+            clearTimeout(this.timeouts[i]);
+        }
+        // clear list
+        this.timeouts = [];
+
+        // calculate end time
+        this.gameTime = new Date().getTime() / 1000;
+
+        // reset variables
         this.computerWins = 0;
         this.userWins = 0;
+        this.computerMoves = [];
+        this.userMoves = [];
+        this.gameTime = 0;
+        this.userGuess = 0;
+        this.count = 0;
+        this.log = [];
+        this.computerThrowCount = 0;
+        this.gameStarted = false;
+
+        // generate computer throw
+        //this.generateThrow();
+
+        //display throws
+        //this.getThrows();
 
         document.getElementById('startbtn').style.display = 'block';
         document.getElementById('gamebtns').style.display = 'none';
+
+        // hide computer generated throws div
+        document.getElementById('computerMoves').style.display = 'none';
     },
-    checkWins: function(){
-        if(this.computerWins == 10 || this.userWins == 10){
-            alert("Computer Wins: " + this.computerWins + " User Wins: " + this.userWins);
-            this.computerWins = 0;
-            this.userWins = 0;
+    checkHighScore: function(){
+
+        // dont record high scores if they don't pass the first round
+        if(this.computerThrowCount == 1){
+            return;
+        }
+
+        // check to see if there is a high score
+        if(this.highScoreRounds > this.computerThrowCount || this.highScoreRounds == -1){
+            if(this.gameTime < this.highScoreTime || this.highScoreTime == -1){
+                this.highScoreRounds = this.computerThrowCount;
+                this.highScoreTime = this.gameTime;
+
+                // add to log
+                this.log.unshift({
+                    msg:"NEW HIGH SCORE | ROUNDS " + (this.highScoreRounds - 1) + " | TIME " + this.highScoreTime.toFixed(2),
+                    playertie: true
+                });
+
+            }
         }
     }
   }
